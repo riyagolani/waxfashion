6dab210a8f982a59a200faa350083a6257c60345
Samyak Meshram <72863327+SamSilmarilData@users.noreply.github.com>
2024-03-26 18:47:02 -0400

    Update SG2_2K_1024_5000kimgs_32.sh

--- slurm_jobs/SG2_2K_1024_5000kimgs_32.sh
+++ slurm_jobs/SG2_2K_1024_5000kimgs_32.sh
@@ -1,25 +1,36 @@
 #!/bin/bash -x
 #SBATCH --job-name=stylegan2
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --output=%j_out.out
 #SBATCH --error=%j_err.out
 #SBATCH --time=158:00:00
 #SBATCH --gres=gpu:2
 
-### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
-### change WORLD_SIZE as gpus/node * num_nodes
-export MASTER_PORT=12340
-export WORLD_SIZE=4
+### Optionally, change 5-digit MASTER_PORT as you wish, ensuring it's not duplicated with others
+### Adjust WORLD_SIZE based on your setup (gpus/node * num_nodes)
+# export MASTER_PORT=<your_choice_of_port>
+# export WORLD_SIZE=<number_of_gpus>
 
-# ### get the first node name as master address
-echo "NODELIST="${SLURM_NODELIST}
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
-
+# ### Uncomment below if you need to get the first node name as master address
+# echo "NODELIST="${SLURM_NODELIST}
+# master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+# export MASTER_ADDR=$master_addr
+# echo "MASTER_ADDR="$MASTER_ADDR
 
 # Activate Conda environment
-source /home/PACE/sm98741n/miniconda3/etc/profile.d/conda.sh
-conda activate venv
+# Replace '/path/to/miniconda3' with your actual path to Miniconda3
+source /path/to/miniconda3/etc/profile.d/conda.sh
+conda activate <your_environment_name>
 
-srun python /home/PACE/sm98741n/FashionGAN-capstone/stylegan2-ada-pytorch/trainv1.py --outdir=../exp3_training_runs --cfg=auto --data=/home/PACE/sm98741n/datasets/2KD1024.zip --resume=/home/PACE/sm98741n/FashionGAN-capstone/training_runs/exp3_training_runs/00001-2KD1024-auto2-kimg5000-batch32/network-snapshot-000900.pkl --gpus=4 --batch=32 --kimg=4000 --snap=25
+# Replace with the path to your training script and adjust other parameters as necessary
+srun python /path/to/your/project/stylegan2-ada-pytorch/trainv1.py \
+--outdir=/path/to/output/directory \
+--cfg=auto \
+--data=/path/to/your/dataset.zip \
+--resume=/path/to/your/snapshot_file.pkl \
+--gpus=4 \
+--batch=32 \
+--kimg=4000 \
+--snap=25
+
+# Add any additional notes or comments here
